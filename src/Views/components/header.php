7edc65a4146d02a7ce8922c3354fe267ff41e7df
ttinonin <tinonin935@gmail.com>
2024-12-07 00:36:04 -0300

    Setting CLI to handle flags

--- src/Views/components/header.php
+++ src/Views/components/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost:8000/output.css">
+    <link rel="stylesheet" href="http://127.0.0.1:8000/output.css">
     <title>Blog</title>
 </head>
 <body>
